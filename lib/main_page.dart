f7a42592acca35754aa99e1cbeb97ed56858e002
Venatrix <venatrix945@gmail.com>
2026-02-05 11:54:45 +0100

    05.02.2026

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:material_symbols_icons/symbols.dart';
+import 'package:crypto/crypto.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -10,7 +14,24 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   final inputController = TextEditingController();
-  String? output;
+
+  bool get hasOutput => inputController.text.trim() != "";
+  String getOutput() {
+    if (!hasOutput) return "";
+    final bytes = utf8.encode(inputController.text);
+    final digest = algorithm.convert(bytes);
+    return digest.toString();
+  }
+
+  Hash algorithm = sha256;
+
+  String getAlgorithmName(Hash algorithm) => switch (algorithm) {
+    md5 => "MD5",
+    sha256 => "SHA256",
+    sha512 => "SHA512",
+    sha512256 => "SHA512/256",
+    _ => algorithm.toString(),
+  };
 
   @override
   void dispose() {
@@ -24,33 +45,80 @@ class _MainPageState extends State<MainPage> {
       appBar: AppBar(leading: Icon(Symbols.tag_rounded), title: Text("Hasher")),
 
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: inputController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "raw string",
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            spacing: 8,
+            children: [
+              Row(
+                spacing: 8,
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: inputController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(borderRadius: .circular(16)),
+                        labelText: "raw string",
+                        suffix: hasOutput
+                            ? IconButton(
+                                onPressed: () => setState(() {
+                                  inputController.text = "";
+                                }),
+                                icon: Icon(Symbols.close_rounded),
+                              )
+                            : null,
+                      ),
+                      onChanged: (value) => setState(() {}),
                     ),
                   ),
-                ),
-              ],
-            ),
 
-            if (output != null)
-              Row(
-                children: [
-                  Expanded(child: Text(output!)),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Symbols.copy_all_rounded),
+                  DropdownMenu(
+                    initialSelection: algorithm,
+                    inputDecorationTheme: Theme.of(context).inputDecorationTheme
+                        .copyWith(
+                          border: OutlineInputBorder(
+                            borderRadius: .circular(16),
+                          ),
+                        ),
+                    menuStyle: MenuStyle(
+                      shape: WidgetStatePropertyAll(
+                        RoundedRectangleBorder(borderRadius: .circular(16)),
+                      ),
+                    ),
+                    dropdownMenuEntries: [
+                      for (final algorithm in [md5, sha256, sha512, sha512256])
+                        DropdownMenuEntry(
+                          value: algorithm,
+                          label: getAlgorithmName(algorithm),
+                        ),
+                    ],
+                    onSelected: (value) => setState(() {
+                      algorithm = value ?? algorithm;
+                    }),
                   ),
                 ],
               ),
-          ],
+
+              ListTile(
+                title: Text("Output"),
+                subtitle: Text(getOutput()),
+                trailing: hasOutput
+                    ? IconButton(
+                        tooltip: "Save to clipboard",
+                        onPressed: () async {
+                          await Clipboard.setData(
+                            ClipboardData(text: getOutput()),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text("Saved hash to Clipboard!")),
+                          );
+                        },
+                        icon: Icon(Symbols.content_copy_rounded),
+                      )
+                    : null,
+              ),
+            ],
+          ),
         ),
       ),
     );
